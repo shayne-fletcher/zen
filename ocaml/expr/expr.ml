3b7b7108bf66f3d8b41b4eb532a4b1d75cba44d2
Shayne Fletcher <fletch@happa.demon.co.uk>
2012-08-20 00:15:51 +0100

    Extend a little bit.

--- ocaml/expr/expr.ml
+++ ocaml/expr/expr.ml
@@ -6,19 +6,35 @@ type expr =
   | BinOp of (((int * int) -> int)*expr*expr)  (* '+','-',etc. *)
   ;;
 
+let x : expr =  X ;;
+let one : expr = Const 1 ;;
+let two : expr = Const 2 ;;
+
 (* Evaluate by destructuring. *)
 
 let rec eval : int->expr->int = 
-  fun t x -> 
-    match x with
+  fun t e -> 
+    match e with
     | X -> t
     | Const a -> a
     | BinOp (f,l, r) -> f ((eval t l),(eval t r))
    ;;
 
-(* Evalue x + 2 at x = 3. *)
+let add : (int*int)->int = fun (a, b) -> (+) a b ;;
+let sub : (int*int)->int = fun (a, b) -> (-) a b ;;
+let div : (int*int)->int = fun (a, b) -> (/) a b ;;
+let mul : (int*int)->int = fun (a, b) -> ( * ) a b ;;
 
-let _ = 
-  Printf.printf "x + 2 (at x = 3) = %d\n" 
-     (eval 3 (BinOp ((fun (x, y) -> x + y), X, Const 2))) 
-  ;;
+(* Some evaluations. *)
+
+let _ =
+  let _ = 
+    let _ = 
+
+      Printf.printf "x + 2 (at x = 3) = %d\n" (eval 3 (BinOp (add, x, two))) 
+    in 
+      Printf.printf "x - 2 (at x = 3) = %d\n" (eval 3 (BinOp (sub, x, two))) 
+  in
+      Printf.printf "x * 2 (at x = 3) = %d\n" (eval 3 (BinOp (mul, x, two))) 
+in
+      Printf.printf "x / 2 (at x = 3) = %d\n" (eval 3 (BinOp (div, x, two))) 
