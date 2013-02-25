98cf6c9a432824848f024a8b31e3808c95b5ed44
Shayne Fletcher <shayne.fletcher.50@gmail.com>
2013-02-25 01:25:46 +0000

    Use ocamlc.opt. Need to clarify what's right here

--- ocaml/echo/run_mlfi.cmd
+++ ocaml/echo/run_mlfi.cmd
@@ -1,2 +1,2 @@
-ocamlc -c echo_c.c
-ocamlc -cclib "-custom-crt msvcrtd.lib -implib -link /def:echo.def" -output-obj -I %OCAMLLIB% -o echo-d.dll echo_caml.ml echo_c.obj 
+ocamlc.opt -c echo_c.c
+ocamlc.opt -cclib "-custom-crt msvcrtd.lib -implib -link /def:echo.def" -output-obj -I %OCAMLLIB% -o echo-d.dll echo_caml.ml echo_c.obj 
