f233d05d8523cdeffa4973978fa1b30028b9fb1f
Shayne Fletcher <shayne.fletcher.50@gmail.com>
2013-02-25 01:19:17 +0000

    Apparently this is all you need

--- ocaml/echo/run_mlfi.cmd
+++ ocaml/echo/run_mlfi.cmd
@@ -1,3 +1,2 @@
-cl -c /nologo /Z7 /D_DEBUG /MDd /DWIN32 /D_WINDOWS /D_DLL /W3 /Zm1000 /EHsc /GR /I %OCAMLLIB% echo_c.c
-
+ocamlc -c echo_c.c
 ocamlc -cclib "-custom-crt msvcrtd.lib -implib -link /def:echo.def" -output-obj -I %OCAMLLIB% -o echo-d.dll echo_caml.ml echo_c.obj 
