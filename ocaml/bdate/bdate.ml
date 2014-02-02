895297a499ca024acdb49d706f1cfc5a2d5f2e1b
Shayne Fletcher <shayne.fletcher.50@gmail.com>
2014-02-01 19:33:25 -0500

    Use the correct fiels in string_of_date

--- ocaml/bdate/bdate.ml
+++ ocaml/bdate/bdate.ml
@@ -5,4 +5,4 @@ external boost_gregorian_day_clock_local_day : unit -> t = "caml_boost_gregorian
 let local_day () = boost_gregorian_day_clock_local_day ()
 let string_of_date tm = 
   Printf.sprintf "%04d-%02d-%02d" 
-    (tm.Unix.tm_year+1900) (tm.Unix.tm_mday) (tm.Unix.tm_wday)
+    (tm.Unix.tm_year+1900) (tm.Unix.tm_mon+1) (tm.Unix.tm_mday)
