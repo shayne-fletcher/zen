4b02492259366e8d2698637d4c81daa4da14754e
Shayne Fletcher <shayne.fletcher.50@gmail.com>
2014-04-05 22:39:20 -0400

    bug fix

--- ocaml/statistics/exponentiation_by_squaring.ml
+++ ocaml/statistics/exponentiation_by_squaring.ml
@@ -7,7 +7,7 @@
     integer n we have
 
         x^n = 
-          | n even -> x * (x ^2)^(n / 2)
+          | n even -> (x^2)^(n / 2)
           | n odd -> x * (x^2)^((n - 1) / 2).
 
 *)
@@ -17,6 +17,6 @@ let rec exp_by_squaring x n =
   else if n = 0 then 1
   else if n = 1 then x
   else if n mod 2 = 0 then exp_by_squaring (x*x) (n/2)
-  else (*n is odd*) exp_by_squaring (x*x) ((n - 1) / 2)
+  else (*n is odd*) x * exp_by_squaring (x*x) ((n - 1) / 2)
 
 let _ = Printf.printf "%d\n" (exp_by_squaring 3 2)
