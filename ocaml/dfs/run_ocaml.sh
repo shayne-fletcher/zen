a5cc7ca33a09d1dcda499145101595dcf68cc144
Shayne Fletcher <shayne.fletcher.50@gmail.com>
2014-07-17 15:36:16 -0400

    final functor version

--- ocaml/dfs/run_ocaml.sh
+++ ocaml/dfs/run_ocaml.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 rm *.cmi *.cmx *~ *.o
-ocamlopt.opt -verbose -o dfs dfs.ml
-ocamlopt.opt -verbose -o dfs_fold dfs_fold.ml
-ocamlopt.opt -verbose -o dfs_fold2 dfs_fold2.ml
+# ocamlopt.opt -verbose -o dfs dfs.ml
+#ocamlopt.opt -verbose -o dfs_fold dfs_fold.ml
+# ocamlopt.opt -verbose -o dfs_fold2 dfs_fold2.ml
 #ocamlopt.opt -verbose -o flyd floyd_warshall.ml
+
+ocamlopt.opt -verbose -o dfs_fold graph.mli graph.ml driver.ml
+ocamldoc -d doc -html -stars -sort -colorize-code graph.mli
