7582c2f9700d5b4fd56d6ab3a7abb38cb94abdab
Shayne Fletcher <sfletcher13@bloomberg.net>
2015-05-25 16:15:49 -0400

    little category stuff

---
+++ ocaml/cat/cat.ml
@@ -0,0 +1,13 @@
+let prod (f : 'c -> 'a) (g : 'c -> 'b) 
+  : 'c -> ('a * 'b) = fun x -> (f x, g x)
+
+let twist : 
+  ('a * 'b) -> ('b * 'a) = fun (x, y) -> (y, x)
+
+let ravel (f : 'u -> 'r) (g : 'v -> 's) 
+  : 'u * 'v -> 'r * 's =  fun (x,  y) -> (f x, g y)
+
+type ('a, 'b) sum = | Left of 'a | Right of 'b
+
+let co_product (f : 'a -> 'c) (g : 'b -> 'c)
+  : ('a, 'b) sum -> 'c =  function | Left x -> f x | Right y -> g y
