e604e4d4ed74cce16e50eb45d3fc7d7bd7f29159
Shayne Fletcher <shayne.fletcher.50@gmail.com>
2015-09-21 11:37:31 -0400

    write the simplify function

--- cpp/float/float.cpp
+++ cpp/float/float.cpp
@@ -1,15 +1,18 @@
-//"c:/program files (x86)/Microsoft Visual Studio 14.0/vc/vcvarsall.bat" x46
+//"c:/program files (x86)/Microsoft Visual Studio 14.0/vc/vcvarsall.bat" x64
 //cl /Fefloat.exe /Zi /MDd /EHsc /I d:/boost_1_56_0 float.cpp
 
 #include <string>
 #include <iostream>
 #include <functional>
 #include <algorithm>
+#include <numeric>
+#include <iterator>
+#include <vector>
+#include <tuple>
 
 #include <boost/variant.hpp>
 #include <boost/variant/apply_visitor.hpp>
 
-struct _neg; 
 struct _fix;   
 struct _add;   
 struct _sub; 
@@ -22,7 +25,6 @@ struct _market;
 
 std::ostream& operator << (std::ostream& os, _const const& e);
 std::ostream& operator << (std::ostream& os, _market const& e);
-std::ostream& operator << (std::ostream& os, _neg const& e);
 std::ostream& operator << (std::ostream& os, _fix const& e);
 std::ostream& operator << (std::ostream& os, _add const& e);
 std::ostream& operator << (std::ostream& os, _sub const& e);
@@ -35,7 +37,6 @@ using expression =
   boost::variant<
       _const
     , _market 
-    , boost::recursive_wrapper<_neg>
     , boost::recursive_wrapper<_fix>
     , boost::recursive_wrapper<_add>
     , boost::recursive_wrapper<_sub>
@@ -47,7 +48,6 @@ using expression =
 
 struct _const { double f; };
 struct _market { std::string tag; };
-struct _neg { expression f; };
 struct _fix { std::string d; expression f; };
 struct _add { expression lhs; expression rhs; };
 struct _sub { expression lhs; expression rhs; };
@@ -58,7 +58,6 @@ struct _min { expression lhs; expression rhs; };
 
 std::ostream& operator << (std::ostream& os, _const const& e) { return os << e.f; }
 std::ostream& operator << (std::ostream& os, _market const& e) {return os << "market(\"" << e.tag << "\")"; }
-std::ostream& operator << (std::ostream& os, _neg const& e) { return os << "-(" << e.f << ")"; }
 std::ostream& operator << (std::ostream& os, _fix const& e) { return os << "fix(" << e.d << ", " << e.f << ")"; }
 std::ostream& operator << (std::ostream& os, _add const& e) { return os << e.lhs << " + " << e.rhs; }
 std::ostream& operator << (std::ostream& os, _sub const& e) { return os << e.lhs << " - " << e.rhs; }
@@ -128,7 +127,6 @@ namespace detail {
     expression operator ()(_const const& x) const { return x; }
     expression operator ()(_market const& x) const {  _fix res; res.d = d; res.f = x; return res; }
     expression operator ()(_fix const& x) const { return x; }
-    expression operator ()(_neg const& x) const { _neg res; res.f = fix (d, x.f); return res; }
     expression operator ()(_add const& x) const { _add res; res.lhs = fix (d, x.lhs); res.rhs = fix (d, x.rhs); return res; }
     expression operator ()(_sub const& x) const { _sub res; res.lhs = fix (d, x.lhs); res.rhs = fix (d, x.rhs); return res; }
     expression operator ()(_mul const& x) const { _mul res; res.lhs = fix (d, x.lhs); res.rhs = fix (d, x.rhs); return res; }
@@ -145,13 +143,61 @@ expression fix (std::string const& d, expression const& x) {
   return boost::apply_visitor (detail::fix_visitor (d), x);
 }
 
+namespace detail {
+
+template <class S, class D, class P>
+D filter (S begin, S end, D dst, P p) {
+  auto f = [&](D d, auto x) 
+    { if (p (x)) return *d++ = x; return d; };
+
+  return std::accumulate (begin, end, dst, f);
+}
+
+}//namespace detail
+
+typedef std::tuple<std::string, std::string, double> fixing_t;
+typedef std::vector<fixing_t> fixings_t;
+expression simplify (fixings_t const& fs, expression const& x);
+
+namespace detail {
+
+  struct simplify_visitor : boost::static_visitor<expression> {
+    fixings_t fs;
+    simplify_visitor (fixings_t const& fs) : fs (fs) {}
+    expression operator () (_const const& x) const { return x; }
+    expression operator () (_market const& x) const { return x; }
+    expression operator () (_fix const& x) const { 
+      fixings_t buf;
+      detail::filter (fs.begin (), fs.end ()
+        , std::back_inserter(buf),[&](fixing_t const& f) { 
+          _market const& m = boost::get<_market>(x.f);
+          return m.tag == std::get<0>(f) && x.d == std::get<1>(f); });
+      if (buf.empty ()) return x; return _const { std::get<2>(buf.front())}; }
+    expression operator ()(_add const& x) const { expression lhs = simplify (fs, x.lhs), rhs = simplify (fs, x.rhs); return lhs + rhs; }
+    expression operator ()(_sub const& x) const { expression lhs = simplify (fs, x.lhs), rhs = simplify (fs, x.rhs); return lhs - rhs; }
+    expression operator ()(_mul const& x) const { expression lhs = simplify (fs, x.lhs), rhs = simplify (fs, x.rhs); return lhs * rhs; }
+    expression operator ()(_div const& x) const { expression lhs = simplify (fs, x.lhs), rhs = simplify (fs, x.rhs); return lhs / rhs; }
+    expression operator ()(_max const& x) const { expression lhs = simplify (fs, x.lhs), rhs = simplify (fs, x.rhs); return (max) (lhs, rhs); }
+    expression operator ()(_min const& x) const { expression lhs = simplify (fs, x.lhs), rhs = simplify (fs, x.rhs); return (min) (lhs, rhs); }
+  };
+
+}//namespace detail
+
+expression simplify (fixings_t const& fs, expression const& x) {
+  return boost::apply_visitor (detail::simplify_visitor (fs), x); }
+
 int main () {
 
-  expression asset=market ("IBM");
+  expression asset = market ("IBM");
   expression number_of_shares = cst (100);
-  expression payoff = number_of_shares * fix ("2016-09-18", max(cst (0.5), min (asset / fix ("2015-09-18", asset) - cst (1.0), cst (0.2))));
+  expression payoff = fix ("2016-09-18",
+    number_of_shares * min (cst (0.3), max (asset / fix ("2015-09-18", asset) - cst (1.0), cst (0))));
 
-  std::cout << payoff << std::endl;
+  std::vector<fixing_t> fixings;
+  fixings.push_back (std::make_tuple (std::string ("IBM"), std::string("2016-09-18"), 0.12));
+  fixings.push_back (std::make_tuple (std::string ("IBM"), std::string("2015-09-18"), 0.10));
+  
+  std::cout << simplify (fixings, payoff) << std::endl;
 
   return 0;
 }
