356d045241559c2a4fab85c8415af067404b3702
Shayne Fletcher <shayne.fletcher.50@gmail.com>
2016-04-30 11:06:10 -0400

    fixed this up so it now runs correctly and terminates in 2/100 (s)

--- ocaml/euler/euler3.ml
+++ ocaml/euler/euler3.ml
@@ -33,18 +33,41 @@ let rec sieve (s : int stream) : int stream =
     (* print_int p; print_newline (); *)
     Cons (p, fun () -> sieve (sift p (g ())))
 (*Natural numbers from starting from [n]*)
-let rec from (n : int) : int stream = Cons (n, fun () -> from (n + 1))
+let rec from (n : int) : int stream =  Cons (n, fun () -> from (n + 1))
+(*The (infinite) stream of prime numbers*)
+let primes : int stream = sieve (from 2)
 
 (*Find the largest prime factor of [n] using direct search
   factorization*)
-let n : int = 600851475143
-let primes : int stream = sieve (from 2)
-let prime_factors : int stream = 
-  filter (fun x -> n mod x = 0) 
-  (take_while (fun x -> x <= int_of_float (floor (sqrt (float_of_int n)))) primes)
-let largest (l : int stream) : int =
-  let rec loop curr = function
-    | Nil -> curr
-    | Cons (h, g) -> loop h (g ()) in
-  let res = loop 0 l in if res <> 0 then res else n
-let () = Printf.printf "%d\n" (largest prime_factors)
+let trial (n : int) : int list =
+  let candidates : int stream = 
+    take_while 
+      (fun x -> 
+       x <=  int_of_float (floor (sqrt (float_of_int n)))) 
+      primes in
+  let rec loop (k, acc) = function
+    | Cons (p, g) -> 
+       if p * p > k then 
+         (k , acc)
+       else
+         let rec aux (k, acc) =
+           if k mod p = 0 then
+             aux ((k / p), (p :: acc))
+           else (k, acc) in
+         loop (aux (k, acc)) (g ()) in
+  let i, l = loop (n, []) candidates in
+  if i > 1 then i :: l else l
+(*
+  In the top level:
+    #load "unix.cma" ;;
+
+  To compile:
+    $ ocamlopt.opt -o euler3.exe unix.cmxa euler3.ml
+*)
+let time (f : unit -> 'a ) : 'a = 
+  let t = Unix.gettimeofday () in
+  let res = f () in
+  let () = Printf.printf "Time : %f (s)\n" (Unix.gettimeofday () -. t) in
+  res
+let factors : int list =
+  time (fun () -> trial 600851475143)
