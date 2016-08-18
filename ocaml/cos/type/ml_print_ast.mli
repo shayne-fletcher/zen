8cd044a1bfbedb4bbe0d06cfdcf827228c160175
Shayne Fletcher <shayne@shaynefletcher.org>
2016-08-18 16:55:13 -0400

    qualify Ml_ast.

--- ocaml/cos/type/ml_print_ast.mli
+++ ocaml/cos/type/ml_print_ast.mli
@@ -1,5 +1,5 @@
-val string_of_pattern : pattern -> string
+val string_of_pattern : Ml_ast.pattern -> string
 
-val string_of_expression : expression -> string
+val string_of_expression : Ml_ast.expression -> string
 
-val string_of_toplevel_phrase : toplevel_phrase -> string
+val string_of_toplevel_phrase : Ml_ast.toplevel_phrase -> string
