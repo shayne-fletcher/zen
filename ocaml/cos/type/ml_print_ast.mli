47a9ac77c195822846ec617297e3dc75dad9eb84
Shayne Fletcher <shayne@shaynefletcher.org>
2016-08-18 16:56:48 -0400

    ocamldoc

--- ocaml/cos/type/ml_print_ast.mli
+++ ocaml/cos/type/ml_print_ast.mli
@@ -1,5 +1,10 @@
+(**String representations of abstract syntax*)
+
 val string_of_pattern : Ml_ast.pattern -> string
+(**Produce a string representation of a pattern*)
 
 val string_of_expression : Ml_ast.expression -> string
+(**Produce a string representation of a expression*)
 
 val string_of_toplevel_phrase : Ml_ast.toplevel_phrase -> string
+(**Produce a string representation of a toplevel phrase*)
