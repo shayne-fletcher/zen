907d18fa606f1b04f8dc53e965fbc9f0864f4985
Shayne Fletcher <shayne@shaynefletcher.org>
2017-02-23 16:32:58 -0500

    document the client a little

--- ocaml/mod_gen/chat/client.ml
+++ ocaml/mod_gen/chat/client.ml
@@ -1,16 +1,22 @@
-let screen_lock : Mutex.t = Mutex.create ()
+(*A mutex for writing to the screen*)
+let console_access : Mutex.t = Mutex.create ()
 
+(*[write s] acquires a lock on the console, writes [s] and releases
+  the lock*)
 let write (s : string) : unit  =
-  Mutex.lock screen_lock;
+  Mutex.lock console_access;
   print_string s;
   flush stdout;
-  Mutex.unlock screen_lock
+  Mutex.unlock console_access
 
-let rooms : string list ref = ref []
+(*The server provided token identifying this client*)
 let tok : string option ref = ref None
 
-let rec process_response (input : in_channel) : unit =
+(*The list of rooms the client has subscribed to*)
+let rooms : string list ref = ref []
 
+(*[proccess_response in_ch] performs updates given server responses*)
+let rec process_response (in_ch : in_channel) : unit =
   let try_parse_token resp = 
     let regexp = Str.regexp "[0-9]+" in
     if Str.string_match regexp resp 0 then
@@ -20,35 +26,36 @@ let rec process_response (input : in_channel) : unit =
     let args = Str.split (Str.regexp_string ":") resp in
     if List.length args > 1 && not (List.mem (List.hd args) !rooms) then
       rooms := (List.hd args) :: !rooms in
-
   try
-    (*Read characters from the input channel until a newline is
-      encountered*)
-    let resp = input_line input in
-    (* Printf.printf "Response : %s\n" resp; flush stdout; *)
-    if try_parse_token resp then () else
+    let resp = input_line in_ch in
+    if try_parse_token resp then () 
+    else
       (try_parse_room resp;
        if resp = "" || resp = "OK" then () else
-         write (resp ^ "\n"); process_response input)
+         write (resp ^ "\n"); process_response in_ch)
   with
   | End_of_file -> ()
 
-let process_command 
-    (server_address : Unix.sockaddr) 
-    (command : string) : unit =
-  let in_ch, out_ch = Unix.open_connection server_address in
+(*[process_cmd server cmd] opens a connection to the server, sends it
+  [cmd] invokes [process_response] in whatever the server comes back
+  with*)
+let process_cmd 
+    (server : Unix.sockaddr) 
+    (cmd : string) : unit =
+  let in_ch, out_ch = Unix.open_connection server in
   (match !tok with
   | Some token -> 
-    (* Printf.printf "Sending : %s\n" ("[" ^ token ^ "]" ^ command ^ "\n"); *)
-    output_string out_ch ("[" ^ token ^ "]" ^ command ^ "\n")
-  | None -> output_string out_ch (command ^ "\n"));
+    output_string out_ch ("[" ^ token ^ "]" ^ cmd ^ "\n")
+  | None -> output_string out_ch (cmd ^ "\n"));
   flush out_ch;
   process_response in_ch;
   close_out out_ch
 
-let update server_address () =
+(*[update server] polls the server for any messages not yet seen in
+  this clients subscribed channels. It runs on its own thread*)
+let update (server : Unix.sockaddr) () : unit =
   let call_for_backlog chan =
-    process_command server_address ("/poll " ^ chan) in
+    process_cmd server ("/poll " ^ chan) in
   while true do
     Thread.delay 1.;
     match !tok with
@@ -56,9 +63,11 @@ let update server_address () =
     | Some token -> List.iter call_for_backlog !rooms
   done
 
+(*Main entry point*)
 let _ =
-  let server_address =
-    (try
+  let server =
+    begin
+      try
        let addr = Unix.inet_addr_of_string (Sys.argv.(1))
        and port = int_of_string (Sys.argv.(2)) in
        Unix.ADDR_INET (addr, port)
@@ -67,14 +76,15 @@ let _ =
        let addr = 
          (Unix.gethostbyname (Unix.gethostname ())).Unix.h_addr_list.(0)
        and port = 8086 in
-       Unix.ADDR_INET (addr, port)) in
-    ignore (Thread.create (update server_address) ());
-    while true do
-      ignore (input_line stdin);
-      Mutex.lock screen_lock;
-      print_string "> ";
-      flush stdout;
-      let command = (input_line stdin) in
-      Mutex.unlock screen_lock;
-      process_command server_address command
-    done
+       Unix.ADDR_INET (addr, port) 
+    end in
+  ignore (Thread.create (update server) ());
+  while true do
+    ignore (input_line stdin);
+    Mutex.lock console_access;
+    print_string "$ ";
+    flush stdout;
+    let cmd = input_line stdin in
+    Mutex.unlock console_access;
+    process_cmd server cmd
+  done
