5dba01c5edfd52a0480f2379989e22c2a68f2deb
Shayne Fletcher <shayne@shaynefletcher.org>
2017-04-10 11:52:55 -0400

    Add an ml file for the AST

---
+++ ocaml/mod_gen/chat/request_ast.ml
@@ -0,0 +1,6 @@
+type ast =
+| Ast_connect of string
+| Ast_nick of string * string
+| Ast_join of string * string
+| Ast_privmsg of (string * string) * string
+| Ast_poll of (string * string)
--- ocaml/mod_gen/chat/run_ocaml.cmd
+++ ocaml/mod_gen/chat/run_ocaml.cmd
@@ -1,17 +1,17 @@
 del *.cmi *.cmo request_parser.mli request_parser.ml request_lexer.ml
 
-ocamlc -c request_ast.mli
+ocamlc -c request_ast.mli request_ast.ml
 ocamlyacc request_parser.mly
 ocamllex request_lexer.mll
 
 ::A top-level for testing the server components
 ocamlmktop -thread -o stop.exe unix.cma threads.cma^
-  request_parser.mli request_parser.ml request_lexer.ml ^
+  request_ast.cmo request_parser.mli request_parser.ml request_lexer.ml ^
   server.ml
 
 ::The server itself
 ocamlc -thread -o server.exe unix.cma threads.cma ^
-  request_parser.mli request_parser.ml request_lexer.ml ^
+  request_ast.cmo request_parser.mli request_parser.ml request_lexer.ml ^
   server.ml server_driver.ml
 
 ::A client
