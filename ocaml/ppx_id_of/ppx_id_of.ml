689ce3aaa2566f49b10af44b160951611447a571
Shayne Fletcher <shayne@shaynefletcher.org>
2017-05-03 16:23:35 -0400

    Refine comments, remove redundant code

--- ocaml/ppx_id_of/ppx_id_of.ml
+++ ocaml/ppx_id_of/ppx_id_of.ml
@@ -13,9 +13,7 @@
   type t = A [@id 2] | B of int [@id 4] [@@id_of]
 
   module M = struct
-    type t =
-    | Foo of int [@id 42]
-    | Bar [@id 43] [@@id_of]
+    type t = | Foo of int [@id 42] | Bar [@id 43] [@@id_of]
 
     module N = struct
       type t = Baz [@id 8] | Quux of string * int [@id 7] [@@id_of]
