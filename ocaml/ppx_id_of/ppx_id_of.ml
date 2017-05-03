05c4d00d6b6e88c1007906457d52a3402b8cc9d0
Shayne Fletcher <shayne@shaynefletcher.org>
2017-05-03 14:35:28 -0400

    Add support for structures of modules

--- ocaml/ppx_id_of/ppx_id_of.ml
+++ ocaml/ppx_id_of/ppx_id_of.ml
@@ -61,17 +61,61 @@ let case_of_constructor_declaration :
   AST of an [id_of] function*)
 let structure_item_mapper 
     (mapper : mapper) 
-    (item : structure_item) : structure_item= 
+    (item : structure_item) : structure_item = 
+
   match item with
+  (*Case of a module*)
+  | {pstr_desc = Pstr_eval (_, _); _} ->
+    (* Printf.printf "Seeing a eval structure item\n"; *)
+    default_mapper.structure_item mapper item
+  | {pstr_desc = Pstr_primitive _; _} ->
+    (* Printf.printf "Seeing a primitive structure item\n"; *)
+    default_mapper.structure_item mapper item
+  | {pstr_desc = Pstr_typext _; _} ->
+    (* Printf.printf "Seeing a typext structure item\n"; *)
+    default_mapper.structure_item mapper item
+  | {pstr_desc = Pstr_exception _; _} ->
+    (* Printf.printf "Seeing a exception structure item\n"; *)
+    default_mapper.structure_item mapper item
+  | {pstr_desc = Pstr_module _; _} ->
+    (* Printf.printf "Seeing a module structure item\n"; *)
+    default_mapper.structure_item mapper item
+  | {pstr_desc = Pstr_recmodule _; _} ->
+    (* Printf.printf "Seeing a rec module structure item\n"; *)
+    default_mapper.structure_item mapper item
+  | {pstr_desc = Pstr_modtype _; _} ->
+    (* Printf.printf "Seeing a module type  structure item\n"; *)
+    default_mapper.structure_item mapper item
+  | {pstr_desc = Pstr_open _; _} ->
+    (* Printf.printf "Seeing an open structure item\n"; *)
+    default_mapper.structure_item mapper item
+  | {pstr_desc = Pstr_class _; _} ->
+    (* Printf.printf "Seeing a class structure item\n"; *)
+    default_mapper.structure_item mapper item
+  | {pstr_desc = Pstr_class_type _; _} ->
+    (* Printf.printf "Seeing a class type structure item\n"; *)
+    default_mapper.structure_item mapper item
+  | {pstr_desc = Pstr_include _; _} ->
+    (* Printf.printf "Seeing an include structure item\n"; *)
+    default_mapper.structure_item mapper item
+  | {pstr_desc = Pstr_attribute _; _} ->
+    (* Printf.printf "Seeing an attribute structure item\n"; *)
+    default_mapper.structure_item mapper item
+  | {pstr_desc = Pstr_extension (_, _); _} ->
+    (* Printf.printf "Seeing an extension structure item\n"; *)
+    default_mapper.structure_item mapper item
+
   (*Case of a single inductive type declaration*)
   | { pstr_desc = Pstr_type (_, [type_decl]); pstr_loc} ->
     begin
+      (* Printf.printf "Seeing a declaration of a type 't'\n"; *)
       match type_decl with
         (*Case the type identifer is [t]*)
       | {ptype_name = {txt = "t"; _};
          ptype_kind = Ptype_variant constructor_declarations;
          ptype_attributes;
          _} ->
+        (* Printf.printf "Writing an \"id_of\" for \"t\"\n"; *)
         let (id_of_attrs, rest) =
           List.partition (fun ({txt;_},_) ->txt="id_of") ptype_attributes in
         begin
@@ -105,26 +149,58 @@ let structure_item_mapper
             item
         end
       (*Case the type identifier is something other than [t]*)
-      | _ -> item
+      | _ -> default_mapper.structure_item mapper item
     end
   (*Case this structure item is something other than a single type
     declaration*)
-  | _ -> item
+  | _ -> default_mapper.structure_item mapper item
+
+let module_binding_mapper mapper binding =
+  let b = 
+    {binding with pmb_expr=(mapper.module_expr mapper binding.pmb_expr)} in
+
+  let running=Stack.pop structures in
+  let running' = List.fold_right 
+    (fun ({pstr_desc;pstr_loc} as m) acc ->
+      match pstr_desc with
+      | Pstr_module {pmb_name={txt;_};_} when txt = binding.pmb_name.txt ->
+          {m with pstr_desc = Pstr_module b} :: acc
+      | _ -> m :: acc
+    ) running [] in
+  Stack.push running' structures;
+  b
+
+let module_expr_mapper mapper expr =
+  let {pmod_desc;pmod_loc;pmod_attributes} = expr in
+  match pmod_desc with
+  | Pmod_structure s ->
+    let s' = mapper.structure mapper s in
+    {expr with pmod_desc=Pmod_structure s'}
+  | _ -> default_mapper.module_expr mapper expr
 
 (*[structure_mapper mapper structure] pushes [structure] onto the
   stack, delegates to the [default_mapper] to organize for traversal
   into the contained [structure_item] values and finally, returns the
   [structure] on the top of the stack*)
 let structure_mapper mapper structure =
-  Stack.push structure structures;
-  ignore (default_mapper.structure mapper (Stack.top structures));
-  Stack.pop structures
+  if (List.length structure <> 0) then
+    begin
+      (* Printf.printf "Pushing a structure of %d items onto the stack\n" (List.length structure); *)
+      Stack.push structure structures;
+      ignore (default_mapper.structure mapper (Stack.top structures));
+      (* Printf.printf "Popping a structure of %d items from the stack\n" (List.length (Stack.top structures)); *)
+      Stack.pop structures
+    end
+  else 
+    default_mapper.structure mapper structure
 
 (*[id_of_mapper argv] is a function from a [string list] (arguments)
   producing a [mapper] record with the [structure] and
   [structure_item] fields bound to the functions above*)
 let id_of_mapper argv = {
   default_mapper with
+    module_binding = module_binding_mapper;
+    module_expr = module_expr_mapper;
     structure = structure_mapper;
     structure_item = structure_item_mapper
 }
