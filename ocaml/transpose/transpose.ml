2d32a322f7ff90d902fdbeb74a18650457817e84
Shayne Fletcher <shayne@shaynefletcher.org>
2017-06-14 13:53:03 -0400

    Transpose

---
+++ ocaml/transpose/transpose.ml
@@ -0,0 +1,5 @@
+let rec transpose (ls : 'a list list) : 'a list list  =
+  let rec transpose_rec acc = function
+  | [] | [] :: _ -> List.rev acc
+  | ls -> transpose_rec (List.map (List.hd) ls :: acc) (List.map (List.tl) ls)
+  in transpose_rec [] ls
