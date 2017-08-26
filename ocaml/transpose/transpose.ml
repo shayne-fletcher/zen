298095a9ced0dc2b3f6c55cf5b65ed4a8eb80e2e
Shayne Fletcher <shayne@shaynefletcher.org>
2017-08-26 17:05:30 -0400

    Record IO

--- ocaml/transpose/transpose.ml
+++ ocaml/transpose/transpose.ml
@@ -3,3 +3,4 @@ let rec transpose (ls : 'a list list) : 'a list list  =
   | [] | [] :: _ -> List.rev acc
   | ls -> transpose_rec (List.map (List.hd) ls :: acc) (List.map (List.tl) ls)
   in transpose_rec [] ls
+
