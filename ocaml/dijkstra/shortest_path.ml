90b2696a54d029877e54a0aa7cd02273c4aa99f3
Shayne Fletcher <shayne@shaynefletcher.org>
2018-01-14 13:48:32 -0500

    graph deriving sexp

--- ocaml/dijkstra/shortest_path.ml
+++ ocaml/dijkstra/shortest_path.ml
@@ -1,21 +1,23 @@
 open Core
 
 module type Graph_sig = sig
-  type node
-  type t
+  type node [@@deriving sexp]
+  type t [@@deriving sexp]
   type extern_t
 
   type colors = [ | `White | `Gray | `Black ]
   type 'a state
 
-  val show : t -> extern_t
+  val to_sexp : t -> Sexp.t
+  val of_sexp : Sexp.t -> t
   val of_adjacency : extern_t -> [ | `Ok of t | `Duplicate_key of node ]
+  val to_adjacency : t -> extern_t
 
 end
 
 module type GRAPH = sig
   module type Ord = sig
-    type t
+    type t [@@deriving sexp]
     include Comparable.S with type t := t
   end
 
@@ -29,7 +31,7 @@ end
 
 module Graph : GRAPH = struct
   module type Ord = sig
-    type t
+    type t [@@deriving sexp]
     include Comparable.S with type t := t
   end
 
@@ -42,10 +44,15 @@ module Graph : GRAPH = struct
     type extern_t = (M.t * M.t list) list =
 
     functor (M : Ord) -> struct
-      type node = M.t
+      type node = M.t [@@deriving sexp]
       type extern_t = (node * node list) list
 
-      type t = node list M.Map.t
+      type t = node list M.Map.t [@@deriving sexp]
+
+      let to_sexp g = sexp_of_t g
+      let of_sexp s = t_of_sexp s
+      let to_adjacency g = M.Map.to_alist g
+      let of_adjacency l = M.Map.of_alist l
 
       type colors = [ | `White | `Gray | `Black]
       type 'a state = {
@@ -55,8 +62,6 @@ module Graph : GRAPH = struct
       ; acc : 'a (* user specified type for [fold] *)
       }
 
-      let show g = M.Map.to_alist g
-      let of_adjacency l = M.Map.of_alist l
     end
 
 end
@@ -78,3 +83,5 @@ let g : G.t =
     ] with
   | `Ok g -> g
   | `Duplicate_key c -> failwithf "of_adjacency : duplicate key '%c'" c ()
+
+;; Printf.printf "%s" (Sexp.to_string (G.to_sexp g))
