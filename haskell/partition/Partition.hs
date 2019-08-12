7b9a21403e1758cbeadd5f59f459fc2daa088ffa
Shayne Fletcher <shayne.fletcher@digitalasset.com>
2019-08-11 20:22:19 -0400

    Partition blog

--- haskell/partition/Partition.hs
+++ haskell/partition/Partition.hs
@@ -1,14 +1,12 @@
 
 partition :: [a] -> [[[a]]]
+partition (h : tl) = concatMap (extend h) (partition tl)
+  where
+    extend :: a -> [[a]] -> [[[a]]]
+    extend x [] = [[[x]]]
+    extend x (h : tl) = ((x : h) : tl) : map (h :) (extend x tl)
 partition [] = [[]]
-partition (h : tl) = concatMap (thread h) (partition tl)
-
-thread :: a -> [[a]] -> [[[a]]]
-thread x [] = [[[x]]]
-thread x (h : tl) = ((x : h) : tl) : map (h :) (thread x tl)
 
 main :: IO ()
 main = do
   putStrLn $ show (partition [1, 2, 3])
-  putStrLn $ show (thread 1 [[2, 3]])
-  putStrLn $ show (thread 1 [[3], [2]])
