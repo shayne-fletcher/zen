61d1d515b8361ecfd96a841c8f6e97d01471ba96
Shayne Fletcher <shayne@shaynefletcher.org>
2020-05-16 09:10:22 -0400

    spacing

--- haskell/number_to_words/Number.hs
+++ haskell/number_to_words/Number.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE TypeApplications #-}
+{-# LANGUAGE LambdaCase #-}
 
 module Main where
 
@@ -24,50 +25,50 @@ split n = reverse (zip (go n) units)
       | otherwise = n `mod` 1000 : (go $ n `div` 1000)
 
 ones :: Int -> String
-ones n
-  -- | n == 0 = "zero"
-  | n == 1 = "one"
-  | n == 2 = "two"
-  | n == 3 = "three"
-  | n == 4 = "four"
-  | n == 5 = "five"
-  | n == 6 = "six"
-  | n == 7 = "seven"
-  | n == 8 = "eight"
-  | n == 9 = "nine"
-  | otherwise = error "Impossible"
+ones = \case
+  1 -> "one"
+  2 -> "two"
+  3 -> "three"
+  4 -> "four"
+  5 -> "five"
+  6 -> "six"
+  7 -> "seven"
+  8 -> "eight"
+  9 -> "nine"
+  _ -> error "Impossible"
 
 teens :: Int -> String
-teens n
-  | n == 10 = "ten"
-  | n == 11 = "eleven"
-  | n == 12 = "twelve"
-  | n == 13 = "thirteen"
-  | n == 14 = "fourteen"
-  | n == 15 = "fifteen"
-  | n == 16 = "sixteen"
-  | n == 17 = "seveteen"
-  | n == 18 = "eighteen"
-  | n == 19 = "nineteen"
-  | otherwise = error "Impossible"
+teens = \case
+  10 -> "ten"
+  11 -> "eleven"
+  12 -> "twelve"
+  13 -> "thirteen"
+  14 -> "fourteen"
+  15 -> "fifteen"
+  16 -> "sixteen"
+  17 -> "seveteen"
+  18 -> "eighteen"
+  19 -> "nineteen"
+  _ -> error "Impossible"
+
 
 tens :: Int -> String
-tens n
-  | n == 2 = "twenty"
-  | n == 3 = "thirty"
-  | n == 4 = "fourty"
-  | n == 5 = "fifty"
-  | n == 6 = "sixty"
-  | n == 7 = "seventy"
-  | n == 8 = "eighty"
-  | n == 9 = "ninety"
-  | otherwise = error "Impossible"
+tens = \case
+  2 -> "twenty"
+  3 -> "thirty"
+  4 -> "fourty"
+  5 -> "fifty"
+  6 -> "sixty"
+  7 -> "seventy"
+  8 -> "eighty"
+  9 -> "ninety"
+  _ -> error "Impossible"
 
 toWords n
   | n == 0 = "zero"
   | n < 10 = ones n
   | n < 20 = teens n
-  | n < 100 = tens (n `div` 10) ++ test (n `mod` 10) ones
+  | n < 100 = tens (n `div` 10) ++ " " ++ test (n `mod` 10) ones
   | n < 1000 = ones (n `div` 100) ++ " hundred " ++ test (n `mod` 100) toWords
   | otherwise = intercalate " " [ test amt $ (++ (" " ++ unit)) . toWords | (amt, unit) <- split n ]
   where test rem f = if rem > 0 then f rem else ""
