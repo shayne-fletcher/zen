059884051b3bd888f1dff704b7e2ac807538377b
Shayne Fletcher <shayne@shaynefletcher.org>
2020-08-18 16:07:42 -0400

    no implicit prelude

--- haskell/ip/Ip.hs
+++ haskell/ip/Ip.hs
@@ -1,8 +1,10 @@
 {-# LANGUAGE ImplicitParams #-}
+{-# LANGUAGE NoImplicitPrelude #-}
 
 module Main where
 
 import Data.List (sortBy)
+import Prelude hiding (min)
 
 sort :: (?cmp :: a -> a -> Ordering) => [a] -> [a]
 sort = sortBy ?cmp
@@ -10,7 +12,7 @@ sort = sortBy ?cmp
 least :: (?cmp::a -> a -> Ordering) => [a] -> a
 least xs = head (sort xs)
 
-min_ :: Ord a => [a] -> a
-min_ = let ?cmp = compare in least
+min :: Ord a => [a] -> a
+min = let ?cmp = compare in least
 
-main = print $ min_ [4, 5, 3]
+main = print $ min [4, 5, 3]
