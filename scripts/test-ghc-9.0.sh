f1fd881f338df58893b9658350e6035c87118f64
Shayne Fletcher <shayne@shaynefletcher.org>
2021-02-28 14:44:21 -0500

    improvements

--- scripts/test-ghc-9.0.sh
+++ scripts/test-ghc-9.0.sh
@@ -4,36 +4,52 @@ set -euxo pipefail
 
 if [ -z "$1" ]
 then
+    echo "Missing ghc-version"
+    exit 1
+fi
+if [ -z "$2" ]
+then
     echo "Missing version-tag"
     exit 1
 fi
 
-version_tag=$1
+ghc_version=$1
+PATH="$HOME/$ghc_version/bin:$PATH"
+export PATH
 
 echo "cabal-install: $(which cabal)"
 echo "cabal-install version: $(cabal -V)"
 echo "ghc: $(which ghc)"
 echo "ghc version : $(ghc -V)"
 
+version_tag=$2
 ghc_lib_dir=~/project/sf-ghc-lib
-ghc_lib_parser_ex_dir=~/project/ghc_lib_parser_ex
-build_dir=~/tmp/sf-ghc-lib/ghc-9.0-test-build
+ghc_lib_parser_ex_dir=~/project/ghc-lib-parser-ex
+build_dir=~/tmp/ghc-lib/$version_tag
 
-mkdir -p $build_dir
-cd $build_dir
-cp $ghc_lib_dir/ghc-lib-parser-$1.tar.gz .
-cp $ghc_lib_dir/ghc-lib-$1.tar.gz .
-cp $ghc_lib_parser_ex_dir/ghc-lib-parser-ex$1.tar.gz .
+mkdir -p $build_dir/$ghc_version
+cd $build_dir/$ghc_version
+cp $ghc_lib_dir/ghc-lib-parser-$version_tag.tar.gz .
+cp $ghc_lib_dir/ghc-lib-$version_tag.tar.gz .
+cp $ghc_lib_parser_ex_dir/ghc-lib-parser-ex-$version_tag.tar.gz .
 gunzip *.gz
 for f in $(ls *.tar)
 do
     tar xvf $f
     rm $f
 done
+cp -R $ghc_lib_dir/examples/mini-hlint ./mini-hlint-$version_tag
+cp -R $ghc_lib_dir/examples/mini-compile ./mini-compile-$version_tag
 cat > cabal.project<<EOF
-packages: ghc-lib-parser-$1,ghc-lib-$1,ghc-lib-parser-ex-$1
+packages:  ghc-lib-parser-$version_tag
+         , ghc-lib-$version_tag
+         , ghc-lib-parser-ex-$version_tag
+         , mini-hlint-$version_tag
+         , mini-compile-$version_tag
 package ghc-lib-parser-ex
-  flags: -auto -no-ghc-lib
+    flags: -auto -no-ghc-lib
 EOF
 rm -rf dist-newstyle
 cabal new-build all
+cabal new-run mini-hlint -- mini-hlint-$version_tag/test/MiniHlintTest.hs
+cabal new-run mini-compile -- mini-compile-$version_tag/test/MiniCompileTest.hs | tail -10
