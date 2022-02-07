9bf16c9326f5cd836bd24df2cfcc4e2afb6d0d1b
Shayne Fletcher <shayne@shaynefletcher.org>
2022-02-06 20:42:41 -0500

    simplification

--- haskell/kind/kind.hs
+++ haskell/kind/kind.hs
@@ -2,12 +2,11 @@
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE TypeApplications #-}
 {-# LANGUAGE StandaloneKindSignatures #-}
-{-# LANGUAGE ScopedTypeVariables #-}
 
 import Data.Kind
 
 type Id :: k -> k  -- kind signature: k is a kind var
-type Id (x :: k) = x  -- definition of `Id`
+type Id x = x  -- definition of `Id`
 
 type Foo :: Bool
 type Foo = Id @Bool False -- `Foo` is type `False` of kind `Bool`
