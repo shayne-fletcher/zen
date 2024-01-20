c5030debbe19ff41e234c50c8b76c5891bc11b31
Shayne Fletcher <shayne@shaynefletcher.org>
2024-01-20 16:52:57 -0500

    refine opam_env.sh

--- bash/opam_env.sh
+++ bash/opam_env.sh
@@ -4,23 +4,28 @@
 
 set -euo pipefail
 
-input=$(opam env --sexp)
+if [ ! "${BASH_VERSINFO:-0}" -ge 4 ]; then
+    echo "require bash >= 4.0 for associative arrays"
+    exit 2
+fi
 
 declare -A env
+
+input=$(opam env --sexp)
+# Remove parens
 input="${input:1:-1}"
 IFS=$'\n';set -f;lines=($input);set +f;IFS=$' \t\n'
 for line in "${lines[@]}"; do
-  # Remove parens
-  line="${line//(/}"
-  line="${line//)/}"
   # Remove leading & trailing white space
   line=$(echo "$line" | sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//')
+  # Remove parens
+  line=${line:1:-1}
   # Split on space
   key="${line%% *}"
   value="${line#* }"
   # Remove quotes
-  key="${key//\"/}"
-  value="${value//\"/}"
+  key=${key:1:-1}
+  value=${value:1:-1}
   # Store the definition
   env[$key]=$value
 done
