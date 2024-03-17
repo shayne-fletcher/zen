9f4ac6bf38e06c9de860c45d10ccb27a9c9c9dd2
Shayne Fletcher <shayne@shaynefletcher.org>
2024-03-17 18:06:06 -0400

    cute program from reddit today

---
+++ haskell/another-diagonal-problem/main.hs
@@ -0,0 +1,21 @@
+{-
+  1, 2, 3
+  4, 5, 6
+  7, 8, 0
+
+  1, 2, 3
+     4, 5, 6
+        7, 8, 9
+-}
+diag :: [[a]] -> [[a]]
+diag [] = []
+diag [r] = map (:[]) r
+diag (r : rs) = zipWith (:) r ([]:ds) ++ drop (length r - 1) ds
+  where ds = diag rs
+main = do
+ let m = [
+         [1, 2, 3]
+       , [4, 5, 6]
+       , [7, 8, 9]
+       ]
+ putStrLn$ show (diag m)
