a4f9682c22305c120498c815b560c928a0792cd3
Shayne Fletcher <shayne@shaynefletcher.org>
2024-03-17 22:44:42 -0400

    view pattern

--- haskell/another-diagonal-problem/main.hs
+++ haskell/another-diagonal-problem/main.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE ViewPatterns #-}
+
 import Data.Foldable(traverse_)
 
 {-
@@ -5,20 +7,38 @@ import Data.Foldable(traverse_)
   4, 5, 6
   7, 8, 9
 
-  1, 2, 3
-     4, 5, 6
-        7, 8, 9
+  1 2 3
+    4 5 6
+      7 8 9
+
+  1 2 3 6 9
+    4 5 8
+      7
 
       3
     2 5 6
   1 4 7 8 9
 
+  1
+  2, 4
+  3, 5, 7
+  6, 8
+  9
+
+        1
+     2, 4
+  3, 5, 7
+     6, 8
+        9
+
+
 -}
 diag :: [[a]] -> [[a]]
 diag [] = []
 diag [r] = map (:[]) r
-diag (r : rs) = zipWith (:) r ([]:ds) ++ drop (length r - 1) ds
-  where ds = diag rs
+diag (r : (diag -> ds)) = zipWith (:) r ([]:ds) ++ [last ds]
+
+main :: IO ()
 main = do
  let m = [
          [1, 2, 3]
