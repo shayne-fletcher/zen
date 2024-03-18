4b3c49824724a59297a6b3b636279fa726924a68
Shayne Fletcher <shayne@shaynefletcher.org>
2024-03-18 07:11:08 -0400

    fix bug

--- haskell/another-diagonal-problem/main.hs
+++ haskell/another-diagonal-problem/main.hs
@@ -2,41 +2,10 @@
 
 import Data.Foldable(traverse_)
 
-{-
-  1, 2, 3
-  4, 5, 6
-  7, 8, 9
-
-  1 2 3
-    4 5 6
-      7 8 9
-
-  1 2 3 6 9
-    4 5 8
-      7
-
-      3
-    2 5 6
-  1 4 7 8 9
-
-  1
-  2, 4
-  3, 5, 7
-  6, 8
-  9
-
-        1
-     2, 4
-  3, 5, 7
-     6, 8
-        9
-
-
--}
 diag :: [[a]] -> [[a]]
 diag [] = []
 diag [r] = map (:[]) r
-diag (r : (diag -> ds)) = zipWith (:) r ([]:ds) ++ [last ds]
+diag (r : (diag -> ds)) = zipWith (:) r ([]:ds) ++ drop (length r - 1) ds
 
 main :: IO ()
 main = do
