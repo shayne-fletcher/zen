a0ee805e0d317c66c5306a47c9bb88283058e58b
Shayne Fletcher <shayne@shaynefletcher.org>
2024-04-12 20:17:51 -0400

    tweaks

--- rust/async/echo/client/src/main.rs
+++ rust/async/echo/client/src/main.rs
@@ -6,6 +6,7 @@ use tokio::net::TcpStream;
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
     let mut stream = TcpStream::connect("127.0.0.1:8080").await?;
+    println!("echo client connected to server. type some lines. type crtl+D to exit.");
 
     let mut stdout = tokio::io::stdout();
     let mut reader = tokio::io::BufReader::new(tokio::io::stdin());
@@ -19,18 +20,20 @@ async fn main() -> std::io::Result<()> {
         let mut line = Vec::new();
         let n = reader.read_until(b'\n', &mut line).await?;
         if n == 0 {
+            println!("ctrl+d = exit");
             break;
         }
         if *line.first().unwrap() == b'\n' {
             continue;
         }
+        line.truncate(line.len() - 1);
 
-        stream.write_all(&line[..n - 1]).await?;
+        stream.write_all(&line).await?;
 
         let mut buf = [0; 1024];
         let n = stream.read(&mut buf).await?;
         stdout.write_all(&buf[..n]).await?;
-        stdout.flush().await.unwrap();
+        stdout.flush().await?;
         loop {
             match stream.try_read(&mut buf) {
                 Ok(0) => break,
@@ -41,16 +44,14 @@ async fn main() -> std::io::Result<()> {
                 }
                 Err(ref e) if e.kind() == tokio::io::ErrorKind::WouldBlock => {
                     stdout.write_all(b"\n").await?;
-                    stdout.flush().await.unwrap();
+                    stdout.flush().await?;
                     break;
                 }
-                Err(e) => {
-                    return Err(e.into());
-                }
+                Err(e) => return Err(e.into()),
             }
         }
         buf.iter_mut().for_each(|m| *m = 0);
     }
 
-    Ok::<(), std::io::Error>(())
+    Ok(())
 }
