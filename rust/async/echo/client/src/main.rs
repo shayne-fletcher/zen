4f0a1da04aa405cb807a8b578ff328a5dd4f97b0
Shayne Fletcher <shayne@shaynefletcher.org>
2024-04-12 21:12:23 -0400

    use tokio::lines::Lines

--- rust/async/echo/client/src/main.rs
+++ rust/async/echo/client/src/main.rs
@@ -1,56 +1,46 @@
 use tokio::io::AsyncBufReadExt;
 use tokio::io::AsyncReadExt;
 use tokio::io::AsyncWriteExt;
-use tokio::net::TcpStream;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    let mut stream = TcpStream::connect("127.0.0.1:8080").await?;
+    let mut stream = tokio::net::TcpStream::connect("127.0.0.1:8080").await?;
     println!("echo client connected to server. type some lines. type crtl+D to exit.");
 
+    let mut lines = tokio::io::BufReader::new(tokio::io::stdin()).lines();
     let mut stdout = tokio::io::stdout();
-    let mut reader = tokio::io::BufReader::new(tokio::io::stdin());
-
-    //let _ = reader.lines();
 
     loop {
-        stdout.write_all(b"> ").await?;
-        stdout.flush().await.ok();
+        match lines.next_line().await? {
+            None => break,
+            Some(line) if line.len() == 0 => continue,
+            Some(line) if line.len() > 0 => {
+                stream.write_all(&line[..].as_bytes()).await?;
 
-        let mut line = Vec::new();
-        let n = reader.read_until(b'\n', &mut line).await?;
-        if n == 0 {
-            println!("ctrl+d = exit");
-            break;
-        }
-        if *line.first().unwrap() == b'\n' {
-            continue;
-        }
-        line.truncate(line.len() - 1);
-
-        stream.write_all(&line).await?;
-
-        let mut buf = [0; 1024];
-        let n = stream.read(&mut buf).await?;
-        stdout.write_all(&buf[..n]).await?;
-        stdout.flush().await?;
-        loop {
-            match stream.try_read(&mut buf) {
-                Ok(0) => break,
-                Ok(n) => {
-                    stdout.write_all(&buf[..n]).await?;
-                    stdout.flush().await.unwrap();
-                    buf.iter_mut().for_each(|m| *m = 0);
+                let mut buf = [0; 1024];
+                let n = stream.read(&mut buf).await?;
+                stdout.write_all(&buf[..n]).await?;
+                stdout.flush().await?;
+                loop {
+                    match stream.try_read(&mut buf) {
+                        Ok(0) => break,
+                        Ok(n) => {
+                            stdout.write_all(&buf[..n]).await?;
+                            stdout.flush().await.unwrap();
+                            buf.iter_mut().for_each(|m| *m = 0);
+                        }
+                        Err(ref e) if e.kind() == tokio::io::ErrorKind::WouldBlock => {
+                            stdout.write_all(b"\n").await?;
+                            stdout.flush().await?;
+                            break;
+                        }
+                        Err(e) => return Err(e.into()),
+                    }
                 }
-                Err(ref e) if e.kind() == tokio::io::ErrorKind::WouldBlock => {
-                    stdout.write_all(b"\n").await?;
-                    stdout.flush().await?;
-                    break;
-                }
-                Err(e) => return Err(e.into()),
+                buf.iter_mut().for_each(|m| *m = 0);
             }
+            Some(_) => panic!(),
         }
-        buf.iter_mut().for_each(|m| *m = 0);
     }
 
     Ok(())
